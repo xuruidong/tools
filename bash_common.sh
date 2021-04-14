3f0d927531bed7101bdfa448f0cb43e57d3588da
xuruidong <xuruidong@gmail.com>
2021-04-14 19:00:54 +0800

    bash_common.sh  cidr<-->mask

--- bash_common.sh
+++ bash_common.sh
@@ -18,3 +18,21 @@ function config_load()
 	fi
 }
 
+# from https://blog.csdn.net/Maestro_T/article/details/108379780
+# 255.255.0.0  -> 16
+mask2cidr ()
+{
+    # Assumes there's no "255." after a non-255 byte in the mask
+    local x=${1##*255.}
+    set -- 0^^^128^192^224^240^248^252^254^ $(( (${#1} - ${#x})*2 )) ${x%%.*}
+    x=${1%%$3*}
+    echo $(( $2 + (${#x}/4) ))
+}
+# 16 -> 255.255.0.0
+cidr2mask ()
+{
+    # Number of args to shift, 255..255, first non-255 byte, zeroes
+    set -- $(( 5 - ($1 / 8) )) 255 255 255 255 $(( (255 << (8 - ($1 % 8))) & 255 )) 0 0 0
+    [ $1 -gt 1 ] && shift $1 || shift
+    echo ${1-0}.${2-0}.${3-0}.${4-0}
+}
